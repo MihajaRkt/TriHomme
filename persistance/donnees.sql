239c2f159d414cb859d936b33592ce2bf9096437
RAJAONSON Ny Andry Miaro <miaromiaro207@gmail.com>
2026-02-09 15:17:08 +0300

    create donnees.sql

---
+++ persistance/donnees.sql
@@ -0,0 +1,7 @@
+--Donnes de test
+
+    --Donnes User
+    INSERT INTO User (nom_User, pwd_User, mail_User) VALUES
+    ('Admin', '1234', 'Admin@admin.admin'),
+    ('Jean', '1234', 'Jean@jean.jean'),
+    ('Bob', '1234', 'Bob@bob.bob');
