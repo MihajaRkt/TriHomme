335ea6e5181fc7675a2dc08ef2695530ed337bb1
RAJAONSON Ny Andry Miaro <miaromiaro207@gmail.com>
2026-02-09 15:46:02 +0300

    Update donnees

--- persistance/donnees.sql
+++ persistance/donnees.sql
@@ -1,9 +1,17 @@
---Donnes de test
+--Donnees de test
 
-    --Donnes User
+    --Donnees User
     INSERT INTO User (nom_User, pwd_User, mail_User) VALUES
-    ('Admin', '1234', 'Admin@admin.admin'),
-    ('Jean', '1234', 'Jean@jean.jean'),
-    ('Bob', '1234', 'Bob@bob.bob');
+    ('Admin', '1234', 'Admin@admin.admin'), 
+    ('Jean', '1234', 'Jean@jean.jean'), 
+    ('Bob', '1234', 'Bob@bob.bob'); 
 
-    --Donnes
+    --Donnees Categorie
+    INSERT INTO Categorie (libelle) VALUES
+    ('Produit menager'), 
+    ('Nourritures'); 
+
+    --Donnees Objet
+    INSERT INTO Objet (id_Proprietaire, id_Categorie, libelle, prix) VALUES
+    (2, 1, 'Balai', 700),
+    (3, 2, 'Chips', 1000);
