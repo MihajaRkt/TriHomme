2bbd7aba8df8260b3b40bb74989537de95b14552
Tendrisoa Nantenaina <ralaiarimangatendry@gmail.com>
2026-02-09 16:58:08 +0300

    modif

--- app/config/routes.php
+++ app/config/routes.php
@@ -3,7 +3,6 @@
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
-use app\controllers\AuthController;
 use app\controllers\AccueilController;
 /** 
  * @var Router $router 
@@ -17,19 +16,7 @@ $router->get('/', function() {
 
 // This wraps all routes in the group with the SecurityHeadersMiddleware
 $router->group('', function (Router $router) use ($app) {
-
-    $router->get('/register', [AuthController::class, 'showRegister']);
-    $router->post('/register', [AuthController::class, 'postRegister']);
-    $router->post('/validate-register', [AuthController::class, 'validateRegisterAjax']);
-
-    $router->get('/login', [AuthController::class, 'showLogin']);
-    $router->post('/login', [AuthController::class, 'postLogin']);
-    $router->post('/validate-login', [AuthController::class, 'validateLoginAjax']);
-
+    
     $router->get('/accueil', [AccueilController::class, 'list']);
-
-    // $message = new MessageController();
-    // $router->get('/message/@id/@id1', [$message, 'charger_messages']);
-    // $router->post('/insertMessage', [$message, 'send_message']);
 
 }, [SecurityHeadersMiddleware::class]);
