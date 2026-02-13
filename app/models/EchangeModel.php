87be7865f8363da4052144f4d922e140e441641a
Tendry-Rlr <ralaiarimangatendry@gmail.com>
2026-02-13 16:11:23 +0300

    reject

--- app/models/EchangeModel.php
+++ app/models/EchangeModel.php
@@ -20,7 +20,7 @@ class EchangeModel
             join User u1 on u1.id_User = ec.idEnvoyeur
             join Objet ob1 on ob1.id_Objet = ec.idObjetEnvoyeur
             join Objet ob2 on ob2.id_Objet = ec.idObjetReceveur
-            WHERE idReceveur = :id";
+            WHERE idReceveur = :id and status is NULL";
 
         $stmt = $this->db->prepare($sql);
         $stmt->execute([':id' => $id_user]);
@@ -36,7 +36,7 @@ class EchangeModel
 
         public function rejectEchange($id_echange)
     {
-        $sql = "update Echanges set statut = 1 where idEchange = :id";
+        $sql = "update Echanges set statut = 0 where idEchange = :id";
         $stmt = $this->db->prepare($sql);
         $stmt->execute([':id' => $id_echange]);
     }
